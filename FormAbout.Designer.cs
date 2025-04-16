00558ca1f3d049a439ed4ae5266e59cca35d7121
soom <s2305@e-mirim.hs.kr>
2025-04-16 14:02:45 +0900

    링크드 라벨 링크 클릭 이벤트 핸들러 구현

--- FormAbout.Designer.cs
+++ FormAbout.Designer.cs
@@ -65,6 +65,7 @@
             this.linkLabel1.TabIndex = 2;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "https://github.com/soom4478/HelloWorldWinform";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // FormAbout
             // 
--- FormAbout.cs
+++ FormAbout.cs
@@ -21,5 +21,10 @@ namespace HelloWorldWinform
         {
 
         }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe", "https://github.com/soom4478 --incognito");
+        }
     }
 }
